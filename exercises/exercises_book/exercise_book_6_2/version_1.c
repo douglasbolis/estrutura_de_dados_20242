3dff88e0bfd9172c2eb9a7560b22728bc259a45b
Douglas Bolis <douglasbolislima@gmail.com>
2024-10-07 09:05:10 -0300

    finisched the book exercise 6_2 adding the function to sum two complex numbers

--- exercises/exercises_book/exercise_book_6_2/version_1.c
+++ exercises/exercises_book/exercise_book_6_2/version_1.c
@@ -6,12 +6,22 @@ typedef struct {
   int b;
 } NumComplexo;
 
+NumComplexo somaComplexos(NumComplexo numX, NumComplexo numY) {
+  NumComplexo res = { numX.a + numY.a, numX.b + numY.b };
+  return res;
+}
+
 int main () {
-  NumComplexo numA = { 0, 0 };
-  NumComplexo numB = { 0, 0 };
+  NumComplexo numA = { 5, 7 };
+  NumComplexo numB = { 3, -12 };
+  NumComplexo soma;
 
   printf("Numero complexo A: %d + %di\n", numA.a, numA.b);
   printf("Numero complexo B: %d + %di\n", numB.a, numB.b);
+
+  soma = somaComplexos(numA, numB);
+
+  printf("Resultado de A + B: %d + %di\n", soma.a, soma.b);
 
   return 0;
 }
