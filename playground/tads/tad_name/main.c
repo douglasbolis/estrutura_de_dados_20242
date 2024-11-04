070a7d0f31b608324876782caef712be8aa1360e
Douglas Lima <douglas@Douglass-Mac-mini.local>
2024-11-04 19:43:56 -0300

    Finished the tad name adding the length of char arrays

--- playground/tads/tad_name/main.c
+++ playground/tads/tad_name/main.c
@@ -9,56 +9,57 @@ int randint(int min, int max) {
   return min + rand() % (max - min + 1);
 }
 
-int in(t_name surname, t_name newSurname[]) {
-  int tam = sizeof(newSurname) / sizeof(newSurname[0]);
-
+int in(t_name surname, t_name newSurname[], int tam) {
   for (int i = 0; i < tam; i++) {
     if (strcmp(surname, newSurname[i]) == 0) {
       return 1;
     }
   }
-
   return 0;
 }
 
-void join(t_name name, t_name newSurname[]) {
-  int tam = sizeof(newSurname) / sizeof(newSurname[0]);
-
-  for(int i = 0; i < tam; i++) {
-    strcat(name, " ");
+void join(t_name name, t_name newSurname[], char charJoin[], int tam) {
+  for (int i = 0; i < tam; i++) {
+    strcat(name, charJoin);
     strcat(name, newSurname[i]);
   }
 }
 
-void generate_fullname(t_name names[], t_name surnames[], int tam, t_name fullname) {
+void generate_fullname(t_name names[], int tamNames, t_name surnames[], int tamSurnames, int tam, t_name fullname) {
   srand(time(NULL));
 
   tam--;
   t_name newSurname[tam];
-  t_name randomName;
   int tamNewSurnames = 0;
-  int tamNames = (int)(sizeof(names) / sizeof(names[0]));
-  int tamSurnames = sizeof(surnames) / sizeof(surnames[0]);
+  
+  // Escolhe um nome aleatório e copia para fullname
+  strcpy(fullname, names[randint(0, tamNames - 1)]);
 
-  strcpy(fullname, names[randint(0, tamNames-1)]);
-
+  // Gera sobrenomes únicos e os armazena em newSurname
   while (tamNewSurnames < tam) {
-    strcpy(randomName, surnames[randint(0, tamSurnames-1)]);
+    t_name randomName;
+    strcpy(randomName, surnames[randint(0, tamSurnames - 1)]);
 
-    if(!in(randomName, newSurname)) {
+    if (!in(randomName, newSurname, tamNewSurnames)) {
       strcpy(newSurname[tamNewSurnames++], randomName);
     }
   }
 
-  join(fullname, newSurname);
+  // Concatena os sobrenomes gerados ao nome completo
+  join(fullname, newSurname, " ", tamNewSurnames);
 }
 
 int main() {
-  t_name fullname = "";
-  t_name names[64] = { "ana", "beatriz", "caio", "douglas", "eliseu", "fabio", "gregori", "helena", "inacio", "jameliz" };
-  t_name surnames[64] = { "andrade", "finazzi", "gomes", "leiva", "menezes", "naves", "oliveria", "rocha", "silva", "yan" };
+  t_name fullname;
+  t_name names[10] = { "ana", "beatriz", "caio", "douglas", "eliseu", "fabio", "gregori", "helena", "inacio", "jameliz" };
+  t_name surnames[10] = { "andrade", "finazzi", "gomes", "leiva", "menezes", "naves", "oliveira", "rocha", "silva", "yan" };
 
-  generate_fullname(names, surnames, 4, fullname);
+  // Tamanho dos arrays names e surnames
+  int tamNames = sizeof(names) / sizeof(names[0]);
+  int tamSurnames = sizeof(surnames) / sizeof(surnames[0]);
+
+  // Gera um nome completo com 1 nome e 3 sobrenomes
+  generate_fullname(names, tamNames, surnames, tamSurnames, 4, fullname);
 
   printf("Nome completo: %s\n", fullname);
 
